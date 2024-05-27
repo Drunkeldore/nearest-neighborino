73b9b029c2aa53c8133eb0ebea51f7a742814878
nickp <nport80@wgu.edu>
2024-05-27 12:03:20 -0400

    Added loadpackagedata to main.py and package class to package.py

---
+++ package.py
@@ -0,0 +1,25 @@
+class Package:
+    """Creates Package Object that takes id, address, city, state, zip, deadline, weight, status, deliveryTime, and departure Time as arguments"""
+    # Created package class to create objects for each package
+    def __init__(self, id, address,city, state, zip, deadline, weight, status):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deadline = deadline
+        self.weight = weight
+        self.status = status
+        # deliveryTime and departure Time will be updated later
+        self.deliveryTime = 0
+        self.deparTime = 0
+
+    #method for updating status of package based on time comparison
+    def status_update(self, time):
+        if (time > deparTime) and (time < deliveryTime):
+            self.status = "In route"
+        elif (time > deliveryTime):
+            self.status = "Delivered"
+        else:
+            self.status = "At the Hub"
+
